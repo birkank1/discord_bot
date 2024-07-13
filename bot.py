ec53d33e3e493b3c2200e572438183324450ad98
birkank1 <birkan.cagan.2009@gmail.com>
2024-07-13 23:07:48 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,31 @@ import discord
 from discord.ext import commands
 import random
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-
-There are a number of utility commands being showcased here.'''
+description = ()
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', description=description, intents=intents)
-#bot sunucuda#
+#BOT ENTERED THE SERVER#
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-#tekrarla#
+    
+#REPEAT SMTHNG#
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
-#___ ne zamandır sunucuda#
+        
+#____ IS IN THE SERVER SINCE ____#
 @bot.command()
 async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
-#komutla resim gönderme#
+    
+#SHOWING IMAGE ON COMMAND#
 @bot.command()
 async def guldur(ctx):
     file_path = 'D:\\python pro derslerim\\discord_bot\\guldur.jpeg'
@@ -38,4 +37,4 @@ async def guldur(ctx):
     
 
 
-bot.run('TOKEN GİRİLMELİ')
+bot.run('')
